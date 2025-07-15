397e97b49ad7df85426d24072ef755c59cf1b6f3
Arushi-Ojha <arushiojha100@gmail.com>
2025-07-15 18:50:27 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,
+        target: process.env.VITE_API_BASE_URL, // or your hardcoded backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
